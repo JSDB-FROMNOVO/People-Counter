24f9f0c8fcaac03a53304fe9ba5905964744bf16
singh264 <amarpreet1997@gmail.com>
2016-07-27 23:32:48 -0400

    fixed upload file save and simplified api links

--- pythonserver/server.py
+++ pythonserver/server.py
@@ -17,8 +17,14 @@ parser.add_argument('data')
 all_files = {
     "TEXT_FILES": [],
     "JSON_FILES": [],
-    "PCAP_FILES": []
+    "CAP_FILES": []
 }
+
+MAP = {"txt": "TEXT_FILES", "json": "JSON_FILES", "cap": "CAP_FILES"}
+
+def file_ext(file_name):
+    extension = file_name.rsplit('.', 1)[1]
+    return MAP[extension] 
 
 
 """ File functions """
@@ -49,55 +55,63 @@ def abort_if_file_doesnt_exist(file_name, file_type):
 
 """ Resource definitions """
 
-# '/files/<string:file_type>/<string:file_name>'
+# '/files/<string:file_name>'
 class files(Resource):
-    # curl http://10.12.1.37:8101/files/see/files
-    def get(self, file_type, file_name):
+    # curl http://10.12.1.37:8101/files/all
+    def get(self, file_name):
         """ List of all test files """
         return all_files
 
-    # curl http://10.12.1.37:8101/files/TEXT_FILES/test.txt -X DELETE -v
-    # curl http://10.12.1.37:8101/files/JSON_FILES/test.json -X DELETE -v
-    def delete(self, file_type, file_name):
+    # curl http://10.12.1.37:8101/files/test.txt -X DELETE -v
+    # curl http://10.12.1.37:8101/files/test.json -X DELETE -v
+    def delete(self, file_name):
         """ Delete text file """
+        file_type = file_ext(str(file_name))
         abort_if_file_doesnt_exist(file_name, file_type)
         delete_file(file_name)
         all_files[file_type].remove(file_name)
         return ("Deleted " + str(file_name)), 204
 
-# '/text_files/text/<string:file_name>'
+# '/data/<string:file_name>'
 class text(Resource):
-    # curl http://10.12.1.37:8101/files/text/test2.txt -X GET -v
+    # curl http://10.12.1.37:8101/data/test1txt -X GET -v
     def get(self, file_name):
         """ Read specific text files 
             TODO: List specific file details
         """
+        file_type = file_ext(str(file_name))
         abort_if_file_doesnt_exist(file_name, file_type)
         return read_file(file_name)
 
-    # curl http://10.12.1.37:8101/files/text/test2.txt -d "data=TEXTDATA" -X POST -v
+    # curl http://10.12.1.37:8101/data/test1.txt -d "data=TEXTDATA" -X POST -v
     def post(self, file_name):
+        file_type = file_ext(str(file_name))
         args = parser.parse_args()
         data = args['data']
         create_file(file_name, data)
-        all_files["TEXT_FILES"].append(file_name)
-        return all_files["TEXT_FILES"]
+        all_files[file_type].append(file_name)
+        return all_files
 
-# '/files/upload/<string:file_type>/<string:file_name>'
+# '/upload/<string:file_name>'
 class upload_file(Resource):
-    # curl -i -X POST -F files=@input.txt http://10.12.1.37:8101/files/upload/TEXT_FILES/test.txt
-    # curl -i -X POST -F files=@sample.json http://10.12.1.37:8101/files/upload/JSON_FILES/test.json
-    def post(self, file_type, file_name):
+    # curl -i -X POST -F files=@input.txt http://10.12.1.37:8101/upload/test2.txt
+    # curl -i -X POST -F files=@sample.json http://10.12.1.37:8101/upload/test2.json
+    def post(self, file_name):
+        file_type = file_ext(str(file_name))
         file_data = request.files['files']
-        print file_data
-        file_data.save('/Users/Amar/Desktop/ugradproj/server/pythonserver/'+file_name)
+        file_data.save(os.path.join('/Users/Amar/Desktop/ugradproj/server/pythonserver', file_name))
         all_files[file_type].append(file_name)
         return all_files
 
 
-api.add_resource(files, '/files/<string:file_type>/<string:file_name>')
-api.add_resource(text, '/textfiles/<string:file_name>')
-api.add_resource(upload_file, '/files/upload/<string:file_type>/<string:file_name>')
+# api.add_resource(files, '/files/<string:file_type>/<string:file_name>')
+# api.add_resource(text, '/textfiles/<string:file_name>')
+# api.add_resource(upload_file, '/files/upload/<string:file_type>/<string:file_name>')
+
+api.add_resource(files, '/files/<string:file_name>')
+api.add_resource(text, '/data/<string:file_name>')
+api.add_resource(upload_file, '/upload/<string:file_name>')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
